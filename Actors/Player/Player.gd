b869f692977dbefc170e6fe4785766e597d628cd
Ian Page Hands <iphands@gmail.com>
2021-02-16 15:41:39 -0500

    refactor and optimize so things are set on just pressed

--- Actors/Player/Player.gd
+++ Actors/Player/Player.gd
@@ -1,28 +1,24 @@
 extends KinematicBody2D
 
+var is_duocorn : bool = false
+var mid_jump : int = 0
 var score : int = 0
-
-var base_speed : int = 250
-var max_speed : int = base_speed * 3
-var max_speed_close = max_speed * 0.80
-var speed = base_speed
-var jump_force : int = 600
-var jump_force_extra : int = jump_force * 0.030
-var gravity : int = 2000
-
-var velocity : Vector2 = Vector2()
-var mid_jump = 0
+var speed : int = base_speed
 var start_position: Vector2 = Vector2()
-var is_duocorn = false
+var velocity : Vector2 = Vector2()
 
-var forward_old = true
-var forward = true
+const base_speed : int = 250
+const max_speed : int = base_speed * 3
+const max_speed_close = max_speed * 0.80
+const jump_force : int = 600
+const jump_force_extra : float = jump_force * 0.030
+const gravity : int = 2000
 
 onready var duo_sprite : AnimatedSprite = get_node("DuocornSprite")
 onready var uni_sprite : AnimatedSprite = get_node("UnicornSprite")
 onready var sprite : AnimatedSprite = duo_sprite
-onready var platform : CollisionPolygon2D = get_node("CollisionPolygon2D")
-onready var particles : Particles2D = get_node("Particles2D")
+onready var platform : CollisionShape2D = get_node("CollisionShape2D")
+onready var particles : Particles2D = get_node("DuocornSprite/Particles2D")
 
 func sprite_handle_jumping():
 	# if !is_on_floor() and !Input.is_action_pressed("jump"):
@@ -38,16 +34,11 @@ func sprite_try_run():
 
 func _ready():
 	start_position = position
-	print(jump_force)
-	print(jump_force_extra)
-	print(start_position)
 	do_duocorn(false)
-	print(self.get_path())
 
 func reset():
 	particles.restart()
 	do_duocorn(false)
-	base_speed = 250
 	position = start_position
 	speed = base_speed
 	velocity.y = 0
@@ -56,20 +47,21 @@ func reset():
 func do_duocorn(b : bool):
 	if b:
 		sprite = duo_sprite
-		# particles.emitting = false
-		# particles.visible = true
-		# particles.restart()
 		is_duocorn = true
 		duo_sprite.visible = true
-		uni_sprite.visible = false 
+		uni_sprite.visible = false
 	else:
 		sprite = uni_sprite
-		# particles.restart()
 		particles.emitting = false
-		# particles.visible = false
 		is_duocorn = false
 		uni_sprite.visible = true
 		duo_sprite.visible = false
+
+func flip_stuff(dir):
+	sprite.flip_h = false if (dir == -1) else true
+	particles.process_material.direction.x = dir
+	particles.transform[2] = Vector2(-12 * dir, -18)
+	particles.rotation_degrees = (-45 * dir)
 
 func _physics_process(delta):
 	var run_speed = speed
@@ -82,7 +74,7 @@ func _physics_process(delta):
 
 	if (Input.is_action_pressed("run") and 
 		(Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right"))):
-		speed *= 1.02
+		speed *= 1.05
 		if speed > max_speed:
 			speed = max_speed
 		sprite.speed_scale = (speed / base_speed)
@@ -93,21 +85,12 @@ func _physics_process(delta):
 	sprite_try_run()
 	sprite_handle_jumping()	
 
-	forward_old = forward
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= run_speed
-		sprite.flip_h = true
-		particles.process_material.direction.x = 1
-		particles.transform[2] = Vector2(-9, -9)
-		forward = true
-	elif Input.is_action_pressed("move_right"):
-		sprite.flip_h = false
-		velocity.x += run_speed
-		particles.process_material.direction.x = -1
-		particles.transform[2] = Vector2(9, -9)
-		forward = false
-	elif !Input.is_action_pressed("jump"):
-		sprite.play("default")
+	if Input.is_action_just_pressed("move_left"):    flip_stuff(1)
+	elif Input.is_action_just_pressed("move_right"): flip_stuff(-1)
+
+	if Input.is_action_pressed("move_left"):    velocity.x -= run_speed
+	elif Input.is_action_pressed("move_right"): velocity.x += run_speed
+	elif !Input.is_action_pressed("jump"):      sprite.play("default")
 
 	velocity = move_and_slide(velocity, Vector2.UP)
 	velocity.y += gravity * delta
@@ -121,5 +104,4 @@ func _physics_process(delta):
 	elif Input.is_action_pressed("jump") and !is_on_floor():
 		velocity.y -= jump_force_extra
 		if is_duocorn: velocity.y -= jump_force_extra * 0.75
-	if position.y > 220:
-		reset()
+	if position.y > 220: reset()
