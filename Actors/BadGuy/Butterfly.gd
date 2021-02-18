a79de72a351ae31336c85b36bcdb0a0412b1603a
Ian Page Hands <iphands@gmail.com>
2021-02-18 11:20:48 -0600

    fixing butterfly colors and opt

--- Actors/BadGuy/Butterfly.gd
+++ Actors/BadGuy/Butterfly.gd
@@ -1,24 +1,20 @@
 extends AnimatedSprite
 
 var target = Vector2(rand_range(-9000, 9000), -5000)
-
-
+const colors = [
+	Color(0,1,0),
+	Color(1,0.45,0.45),
+	Color(1,0,1),
+	Color(0,0,1),
+	Color(1,0.25,1),
+	Color(1,1,0.25),
+	Color(1,0,0),
+	Color(1,0.25,0.75),
+]
 
 func _ready():
 	flip_h = global_position.x > target.x
-	var colors = [
-		Color(0,1,0),
-		Color(1,0.45,0.45),
-		Color(1,0,1),
-		Color(0,0,1),
-		Color(1,0.25,1),
-		Color(1,1,0.25),
-		Color(1,0,0),
-		Color(1,0.75,0.75),
-		Color(1,0.55,0.55),
-	]
-	colors.shuffle()
-	self_modulate = colors[0]
+	self_modulate = colors[rand_range(0, colors.size())]
  
 func _physics_process(_delta):
 	global_position = global_position.move_toward(target, 4)
