906720641e95fe68dc3f71a68f7fbb1f273e43a9
Ian Page Hands <iphands@gmail.com>
2021-02-19 15:25:03 -0500

    renfining rainbow portals

--- PortalArea.gd
+++ PortalArea.gd
@@ -1,8 +1,1 @@
 extends Area2D
-
-func _ready():
-	var _tmp = connect("body_entered", self, "_on_body_enter")
-
-func _on_body_enter(_body):
-	var player = get_node("/root/MainScene/Player")
-	player.do_duocorn(true)
